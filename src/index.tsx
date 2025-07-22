0e59db3d1336dd4742d81c756c421c934a05e1af
Alejandro Zea <aroryt02@gmail.com>
2025-07-22 10:29:41 -0500

    refactor: Enhance root element rendering logic with error handling for improved robustness

--- src/index.tsx
+++ src/index.tsx
@@ -4,14 +4,21 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+} else {
+  throw new Error("No se encontró el elemento root en el HTML.");
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// Para medir el rendimiento, descomenta y pasa una función como argumento:
+// reportWebVitals(console.log);
